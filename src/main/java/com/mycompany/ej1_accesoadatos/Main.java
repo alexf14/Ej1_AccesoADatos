71361decbaf14ca7ef9ce782903b76cff28c9c61
Mkwolf11 <mcf94@iesornia.com>
2021-11-05 18:17:16 +0100

    PETANDO EL PUTO TRABAJO Y ARREGLANDO ERRORES

--- src/main/java/com/mycompany/ej1_accesoadatos/Main.java
+++ src/main/java/com/mycompany/ej1_accesoadatos/Main.java
@@ -4,8 +4,7 @@ import static java.lang.Integer.parseInt;
 import java.util.Scanner;
 
 public class Main {
-    
-    
+
     public static void main(String [] arg){
         Partida p=new Partida();
         Fichero_Facil a = new Fichero_Facil();
@@ -22,17 +21,30 @@ public class Main {
 
         //Si selecciona cargar ua partida se cargara el fichero de guardado con la partida en curso
             //Ir a la clase de juego
+        while(!("c".equals(lPartida) || "C".equals(lPartida)) && !("n".equals(lPartida) || "N".equals(lPartida))){
+            System.out.println("Introduce una letra valida");
+            lPartida= src.next();
             
-        if("c".equals(lPartida) || "C".equals(lPartida)){
+            if("c".equals(lPartida) || "C".equals(lPartida)){
             //Llamar al metodo para cargar partida
-        }else if("n".equals(lPartida) || "N".equals(lPartida)){
-            System.out.println("Pulsa F para facil (de 1 a 15 carácteres)");
-            System.out.println("Pulsa D para dificil (más de 15 carácteres)");
+            }else if("n".equals(lPartida) || "N".equals(lPartida)){
+                System.out.println("Pulsa F para facil (de 1 a 15 carácteres)");
+                System.out.println("Pulsa D para dificil (más de 15 carácteres)");
             
-            String  dPartida= dificultad.next();
-            if("f".equals(dPartida)|| "F".equals(dPartida)){
-                p.partida();
+                String  dPartida= dificultad.next();
+                while(!("f".equals(lPartida) || "F".equals(lPartida)) && !("d".equals(lPartida) || "D".equals(lPartida))){
+                    System.out.println("Introduce una letra valida");
+                    dPartida= dificultad.next();
+                    
+                    if("f".equals(dPartida)|| "F".equals(dPartida)){
+                        p.partida();
+                        break;
+                    }else if("d".equals(dPartida)|| "D".equals(dPartida)){
+                        System.out.println("Estos no hace una puta mierda");
+                    }
+                }
             }
-        }
+        }    
+
     }
 }
